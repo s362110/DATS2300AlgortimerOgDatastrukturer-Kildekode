26fb43c42dd5bb5649641c289c9fa41eac44cd22
s362110 <s362110@oslomet.no>
2022-11-07 14:40:47 +0100

    Implementert litt mer og lagt til til mer kommentarer til tilhørende implementering.

--- src/Uke42_BinæreTrær/BinaryTree.java
+++ src/Uke42_BinæreTrær/BinaryTree.java
@@ -70,8 +70,79 @@ public class BinaryTree {
         }
     }
 
-    // Lager en main funksjon for å skrive ut
+    // Vi skal lage den rekursive måten å skrive ut dybde først.
 
+    // Lager en som er preorden, inorden og en som er postorden
+    static void printPreOrder(BinaryTreeNode node){
+        // Legger til en sjekk dersom noden er null.
+        if (node == null){
+            // Da skal vi ikke gjøre noe men kun returnere.
+            // Dette er det ene kriteriet som vi må ha med rekursive funksjoner.
+            // Også må vi ha et annet kriteriet som er et basiskriteriet som stanser funksjonen.
+            // I dette tilfellet så er dette her basiskriteriet ettersom vi stanser når noden er lik null.
+            return;
+        }
+
+        // Skriver ut preorden
+        System.out.print(node.value + " ");
+        printPreOrder(node.left_child);
+        printPreOrder(node.right_child);
+    }
+
+    static void printInOrder(BinaryTreeNode node){
+        // Legger til en sjekk dersom noden er null.
+        if (node == null){
+            return;
+        }
+
+        // Skriver ut inorden
+        printInOrder(node.left_child);
+        System.out.print(node.value + " ");
+        printInOrder(node.right_child);
+    }
+
+    static void printPostOrder(BinaryTreeNode node){
+        // Legger til en sjekk dersom noden er null.
+        if (node == null){
+            return;
+        }
+
+        // Skriver ut postorden
+        printPostOrder(node.left_child);
+        printPostOrder(node.right_child);
+        System.out.print(node.value + " ");
+    }
+
+    // static void printDepthFirstNonRecursive(BinaryTreeNode root){
+    static void printPreOrderNonRecursive(BinaryTreeNode root){
+        // Bruker en stack for emulere programstacken i Java.
+        ArrayDeque<BinaryTreeNode> stack = new ArrayDeque<BinaryTreeNode>();
+
+        // Legger til roten til stacken
+        // Legger til fra bak og tar ut fra bak
+        stack.addLast(root);
+
+        while(!stack.isEmpty()){
+            // Tar ut fra stacken fra samme sted. Så jobber hele tiden fra hale-enden.
+            BinaryTreeNode current = stack.removeLast();
+
+            // Fikser nullsjekken
+            if (current.right_child != null){
+                // Legger til i stacken
+                // Vi må legge til høyre barn først fordi vi jobber i hale-enden.
+                stack.addLast(current.right_child);
+
+            }
+
+            if (current.left_child != null){
+                stack.addLast(current.left_child);
+            }
+
+            System.out.print(current.value + " ");
+        }
+    }
+
+    // Lager en main funksjon for å skrive ut
     public static void main(String[] args){
         // Lager rot-noden (nivå 0)
         BinaryTreeNode root = new BinaryTreeNode('A');
@@ -87,11 +158,29 @@ public class BinaryTree {
         BinaryTreeNode e = b.addRightChild('E');
 
         BinaryTreeNode f = c.addLeftChild('F');
-        BinaryTreeNode g = d.addRightChild('G');
+        BinaryTreeNode g = c.addRightChild('G');
 
         // Kall funksjonen vår
-        printLevelOrder(root);
+        // printLevelOrder(root);
 
+        // Skriver ut preorden
+        System.out.println("Pre order: ");
+        printPreOrder(root);
+        System.out.println();
 
+        // Skriver ut inorden
+        System.out.println("In order: ");
+        printInOrder(root);
+        System.out.println();
+
+        // Skriver ut postorden
+        System.out.println("Post order: ");
+        printPostOrder(root);
+        System.out.println();
+
+        // Skriver ut preorden ikke-rekursiv
+        System.out.println("Pre order non-recursive: ");
+        printPreOrderNonRecursive(root);
+        System.out.println();
     }
 }
